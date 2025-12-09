519852c2c028a7d587584c3b4824db5cddb8d67b
staarmaan <starman.starry@gmail.com>
2025-12-09 14:28:59 -0800

    Correciones en las horas de entrega de comandas.

--- EnflorarteTopiProyecto/Models/Comanda.cs
+++ EnflorarteTopiProyecto/Models/Comanda.cs
@@ -120,8 +120,10 @@ namespace EnflorarteTopiProyecto.Models
 
         /*
         No se guarda la foto como tal en la bd, sino la ruta donde se encuentra almacenada. Si se guardara la foto en la bd, esta podria crecer mucho en tamaño y hacer las consultas muy lentas.
-        Entonces, cuando se cargue una foto, se guarda una copia para que pueda ser cargada despues.
-        Como por ahora la app es solo local, la ruta puede ser una ruta relativa o absoluta en el sistema de archivos local.
+        Entonces, cuando se cargue una foto, se guarda una copia en alguna carpeta del sistema, y esta puede ser cargada despues.
+        Que sea una copia es lo ideal, porque si se mueve o elimina la foto original, la foto de la comanda no se veria afectada.
+
+        Como por ahora la app es solo local, la ruta puede ser una ruta relativa en el sistema de archivos local.
         Pero si se quisiera hacer una version web, habria que cambiar la logica para que las fotos se guarden en un servidor o servicio de almacenamiento en la nube, donde se puedan cargar las imagenes en cualquier dispositivo.
          */
         public string? FotoArregloRuta { get; set; }
